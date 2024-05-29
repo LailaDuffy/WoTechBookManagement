86ee09f57075efa925ced4df9a8898131ef8510c
Laila <78215618+LailaDuffy@users.noreply.github.com>
2024-05-29 21:02:59 +0300

    Create Book.java

---
+++ Book.java
@@ -0,0 +1,12 @@
+public class Book {
+
+  public String title;
+  public String author;
+  public int publishingYear;
+
+  public Book(String title, String author, int publishingYear){
+    this.title = title;
+    this.author = author;
+    this.publishingYear = publishingYear;
+  }  
+}
